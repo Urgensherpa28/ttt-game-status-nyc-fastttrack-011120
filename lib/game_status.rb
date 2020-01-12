c64e6cb0f4f5fc3f6193bb9d43f9ac6f7e93c2a9
Urgen Sherpa <urgen.nyc@gmail.com>
2020-01-12 17:02:59 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -42,23 +42,23 @@ end
 def draw?(board)
   if won?(board) 
     false
-  elsif !won(board) && full?(board)
+  elsif !won?(board) && full?(board)
     true
   else
     false
   end
 end
 
-# # BROKEN but should be working according to js_tester.rb
-def draw?(board)
-  if won?(board) # BROKEN! If #won? returns an array, return false
-    false
-  elsif !won?(board) && full?(board) # Draw conditions: No win and full board
-    true
-  else
-    false
-  end
-end
+# # # BROKEN but should be working according to js_tester.rb
+# def draw?(board)
+#   if won?(board) # BROKEN! If #won? returns an array, return false
+#     false
+#   elsif !won?(board) && full?(board) # Draw conditions: No win and full board
+#     true
+#   else
+#     false
+#   end
+# end
 
 # # BROKEN definitely broken
 # def over?(board)
