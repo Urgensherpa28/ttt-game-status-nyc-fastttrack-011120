51861050869193aae29ec44d02b7ae665fe2900c
Urgen Sherpa <urgen.nyc@gmail.com>
2020-01-12 16:34:45 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,7 +21,13 @@ def won?(board)
   
   if position_1 == "X" && position_2 == "X" && position_3 == "X"
     return win_combo
- end
+  end
+  
+  if position_1 == "O" && position_2 == "O" && position_3 == "O"
+    return win_combo
+  end
+  
+  false
 end
   
   
